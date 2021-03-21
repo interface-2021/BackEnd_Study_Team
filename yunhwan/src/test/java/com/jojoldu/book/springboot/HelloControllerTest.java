560f53a40aa7d86140efa0ac5b85cab5df3d5377
yunhwan98 <sky10240107@naver.com>
2021-03-21 22:38:23 +0900

    2주차
    2주차 수정

---
+++ yunhwan/src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package com.jojoldu.book.springboot;
+
+
+import com.jojoldu.book.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)//
+@WebMvcTest(controllers = HelloController.class)//
+public class HelloControllerTest {
+
+    @Autowired //
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello ="hello";
+
+        mvc.perform(get("/hello"))//
+                .andExpect(status().isOk())//
+                .andExpect(content().string(hello));
+    }
+
+}
