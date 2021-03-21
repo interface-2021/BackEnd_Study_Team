99051fd15ce545051cd73a234564e559763c904b
Komputer-P <wlghks0314@gmail.com>
2021-03-21 20:18:19 +0900

    Chapter 2. Unit Test

---
+++ jihwan/src/main/java/kom/backendstudy/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package kom.backendstudy.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
