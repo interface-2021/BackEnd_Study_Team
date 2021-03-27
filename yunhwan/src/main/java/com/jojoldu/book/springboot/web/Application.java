ac6aae46023105a9842d671e3891d2db81abf99c
yunhwan98 <sky10240107@naver.com>
2021-03-28 00:42:26 +0900

    2주차
    2주차 완료

--- yunhwan/src/main/java/com/jojoldu/book/springboot/web/Application.java
+++ yunhwan/src/main/java/com/jojoldu/book/springboot/web/Application.java
@@ -1,4 +1,4 @@
-package com.jojoldu.book.springboot;
+package com.jojoldu.book.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- yunhwan/src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ yunhwan/src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,7 +1,7 @@
-package com.jojoldu.book.springboot;
+package com.jojoldu.book.springboot.web;
 
 
-import com.jojoldu.book.springboot.web.HelloController;
+import com.jojoldu.book.springboot.web.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +9,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -29,5 +31,23 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())//
                 .andExpect(content().string(hello));
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+
+    }
+
+
 
 }
