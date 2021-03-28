213fa5e8c8b09b4c70b29a5334ef8bf4df1e9838
SongTaeKwon <79988462+SongTaeKwon@users.noreply.github.com>
2021-03-28 20:55:09 +0900

    3주차

---
+++ src/main/java/com/blog/taekwon/admin/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.blog.taekwon.admin.service.posts;
+
+import com.blog.taekwon.admin.domain.posts.PostsRepository;
+import com.blog.taekwon.admin.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
