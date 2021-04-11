59eae58109490f7e90007d7d0d54a47befffef4c
Komputer-P <wlghks0314@gmail.com>
2021-04-11 20:02:00 +0900

    Chapter 4. UI
     UI Using Mustache

--- jihwan/src/main/java/kom/backendstudy/springboot/service/posts/PostsService.java
+++ jihwan/src/main/java/kom/backendstudy/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package kom.backendstudy.springboot.service.posts;
 
 import kom.backendstudy.springboot.domain.posts.Posts;
 import kom.backendstudy.springboot.domain.posts.PostsRepository;
+import kom.backendstudy.springboot.web.dto.PostsListResponseDto;
 import kom.backendstudy.springboot.web.dto.PostsResponseDto;
 import kom.backendstudy.springboot.web.dto.PostsSaveRequestDto;
 import kom.backendstudy.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +36,19 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
