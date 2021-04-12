1bcb4eb530a4296405e38b2c31b9b3832bb25f3b
Komputer-P <wlghks0314@gmail.com>
2021-04-12 22:04:42 +0900

    Chapter 05. OAuth2
    Social Login using OAuth2

--- jihwan/src/main/java/kom/backendstudy/springboot/web/IndexController.java
+++ jihwan/src/main/java/kom/backendstudy/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package kom.backendstudy.springboot.web;
 
+import kom.backendstudy.springboot.config.auth.dto.SessionUser;
 import kom.backendstudy.springboot.service.posts.PostsService;
 import kom.backendstudy.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,16 +10,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
 
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+            model.addAttribute("userImage", user.getPicture());
+        }
         return "index";
     }
 
